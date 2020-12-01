1b5e1aab5bbddbeebc7c1064e1f9a5bff8adb73d
Joseph Timbol <joseph2000k@gmail.com>
2020-12-01 14:03:18 +0800

    added get method for all members

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const path = require('path');
 
 const app = express();
 
+
+members = [
+    {
+        id: 1,
+        name: 'John',
+        email:  'John@gmail.com',
+        status: 'acitve'
+    }
+];
+
+//Gets all members
+app.get('/api/members', (req, res) => 
+    res.json(members)
+);
+
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
