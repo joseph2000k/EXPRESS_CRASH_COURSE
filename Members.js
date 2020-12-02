9001c7123bae76dc28d1950738c680b9e8905eff
Joseph Timbol <joseph2000k@gmail.com>
2020-12-02 14:33:37 +0800

    get single member (traverse media Express JS Crash Course 33:29)

--- Members.js
+++ Members.js
@@ -3,13 +3,13 @@ const members = [
         id: 1,
         name: 'John',
         email:  'John@gmail.com',
-        status: 'acitve'
+        status: 'active'
     },
     {
         id: 2,
         name: 'Joseph',
         email:  'joseph@gmail.com',
-        status: 'acitve'
+        status: 'active'
     }
 ];
 
